ab3ed24d1a0ff489b336b29748e299f48f6658f5
Henry <61155410+ParkJsoo@users.noreply.github.com>
2022-09-15 22:58:06 +0900

    [section5-14] swiper 모듈을 이용한 터치 슬라이드 구현하기
    - swiper 라이브러리로 loop 터치 슬라이드 구현

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params:  {
     api_key:  '4869f22d7f395baec68e3acb8d9ea9a2',
-    language: 'ko_KR'
+    language: 'ko-KR'
   }
 });
 
