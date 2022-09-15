2e67b515e1d694228e210a15973c395357b01cd8
Henry <61155410+ParkJsoo@users.noreply.github.com>
2022-09-15 23:42:22 +0900

    [section5-15] github를 이용해서 배포하기
    - gh-pages 라이브러리와 github를 이용해 프로젝트 배포

--- src/hooks/useOnClickOutside.js
+++ src/hooks/useOnClickOutside.js
@@ -1,29 +1,22 @@
-import React, {useEffect} from 'react';
+import { useEffect } from 'react';
 
-const UseOnClickOutside = (ref, handler) => {
-  useEffect(() => {
-    const listener = (event) => {
-      console.log('ref', ref.current);
-      if (!ref.current || ref.current.contains(event.target)) {
-        return;
-      }
-      handler();
-    };
+export default function useOnClickOutside(ref, handler) {
 
-    document.addEventListener('mousedown', listener);
-    document.addEventListener('touchstart', listener);
-
-    return () => {
-      document.addEventListener('mousedown', listener);
-      document.addEventListener('touchstart', listener);
-    }
-  }, [])
-
-  return (
-    <div>
-      UseOnClickOutside
-    </div>
+  useEffect(
+    () => {
+      const listener = (event) => {
+        if (!ref.current || ref.current.contains(event.target)) {
+          return;
+        }
+        handler(event);
+      };
+      document.addEventListener("mousedown", listener);
+      document.addEventListener("touchstart", listener);
+      return () => {
+        document.removeEventListener("mousedown", listener);
+        document.removeEventListener("touchstart", listener);
+      };
+    },
+    [ref, handler]
   );
-};
-
-export default UseOnClickOutside;
+}
